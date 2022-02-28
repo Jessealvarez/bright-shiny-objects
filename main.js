32439d940a7ec4e1fe9260b73db3407b49a95cb3
Jesse Alvarez <jesse@Jesses-MBP.home>
2022-02-28 12:23:18 -0500

    finished

--- main.js
+++ main.js
@@ -1,14 +1,68 @@
 // ┌─────────────────┐
 // │ Your code here! │
 // └─────────────────┘
+function createUser(firstName, lastName){
+    let fullName = {
+        firstName: firstName,
+        lastName: lastName
+    }
+    return fullName;
+}
+console.log(createUser('Jimmy', 'Persons').firstName);
 
 
+function setAge(user, age){
+    user.age = age
+    return user;
+}
 
 
+function incrementAge(user){
+    user.age ++;
+    return user;
+}
 
+function fixCar(car){
+    
+    car.needsMaintenance = false;
+    return car;
+}
 
+function addGrades(student, newGrades){
+    for(let i = 0; i < newGrades.length; i++){
+        student.grades.push(newGrades[i])
+    }
+    return student;
+}
 
+function getDataType(car, key){
+   let obj = car[key]
+    return typeof obj;
+}
 
+function addTodo(todos, newTodo){
+    todos.push(newTodo);
+    return todos;
+
+}
+
+function addSong(playlist, newSong){
+    playlist.duration += newSong.duration;
+    playlist.songs.push(newSong);
+    return playlist;
+}
+
+function updateReportCard(reportCard, newGrade){
+    reportCard.grades.push(newGrade)
+    reportCard.lowestGrade = Math.min(...reportCard.grades)
+    reportCard.highestGrade = Math.max(...reportCard.grades)
+        let sum = 0;
+        for(let i =0; i < reportCard.grades.length; i++){
+            sum += reportCard.grades[i];
+        }
+        reportCard.averageGrade = sum / reportCard.grades.length;
+        return reportCard;
+}
 
 
 // ┌─────────────────────────────────────────────────────────────────────────┐
